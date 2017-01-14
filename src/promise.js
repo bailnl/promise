2d0e70629c79c509b01ac6a9d6ca9a8577e14741
bailnl <bailnl@qq.com>
2017-01-14 19:51:51 +0800

    The Promise Resolution Procedure

--- src/promise.js
+++ src/promise.js
@@ -3,14 +3,60 @@ const FULFILLED = 1
 const REJECTED = 2
 
 const isFunction = fn => typeof fn === 'function'
+const isObject = obj => obj !== null && typeof obj === 'object'
 const noop = () => {}
 
 
 const nextTick = (fn) => setTimeout(fn, 0)
 
-const resolve = (promise, value) => {}
+const resolve = (promise, x) => {
+  if (promise === x) {
+
+    reject(promise, new TypeError('You cannot resolve a promise with itself'))
+  } else if (x && x.constructor === Promise) {
+   
+    if (x._stauts === PENDING) {
+      const handler = statusHandler => value => statusHandler(promise, value)         
+      x.then(handler(resolve), handler(reject))        
+    } else if (x._stauts === FULFILLED) {
+      fulfill(promise, x._value)
+    } else if (x._stauts === REJECTED) {
+      reject(promise, x._value)
+    }
+  } else if (isFunction(x) || isObject(x)) {
+
+    let isCalled = false
+
+    try {
+      const then = x.then
+      
+      if (isFunction(then)) {
+
+        const handler = statusHandler => value => {
+            if (!isCalled) {
+              statusHandler(promise, value)
+            }
+            isCalled = true
+          }
+        }
+        then.call(promise, handler(reject), handler(fulfill))
+      } else {
+        fulfill(promise, x)
+      }
+
+    } catch (e) {
+      if (!isCalled)  {
+        reject(promise, e)
+      }
+    }
+
+  } else {
+    fulfill(promise, x)
+  }
+}
 
 const reject = (promise, value) => {}
+const fulfill = (promise, value) => {}
 
 const invokeCallback = (promise) => {
   if (promise._stauts === PENDING) {
