9bed3cabc2861cba0b677ce26d087f8c83411c3b
bailnl <bailnl@qq.com>
2017-01-14 16:42:34 +0800

    implementation then

--- src/promise.js
+++ src/promise.js
@@ -3,6 +3,9 @@ const FULFILLED = 1
 const REJECTED = 2
 
 const isFunction = fn => typeof fn === 'function'
+const noop = () => {}
+
+const invokeCallback = (promise) => {}
 
 class Promise {
   constructor(resolver) {
@@ -12,7 +15,20 @@ class Promise {
 
     this._stauts = PENDING
     this._value = undefined
+    this._callbacks = []    
+  }
 
-    this._subscribers = []    
+  then(onFulfilled, onRejected) {
+    const thenPromise = this.constructor(noop)
+
+    this._callbacks = this._callbacks.concat([{
+      onFulfilled: isFunction(onFulfilled) ? onFulfilled : null,
+      onRejected: isFunction(onRejected) ? onRejected : null,
+      thenPromise,
+    }])
+
+    invokeCallback(this)
+
+    return thenPromise
   }
 }
